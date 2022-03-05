9912c2394144f48d537a8be231d5a0bb4f98814d
kristielGJ <g_jahja31@hotmail.com>
2022-03-05 03:01:46 +0000

    Implemented the dissasembly of Add using visitors and interfaces

--- src/opcodes/add.java
+++ src/opcodes/add.java
@@ -1,9 +1,36 @@
 //Author Gera Jahja
 package src.opcodes;
+import src.interfaces.*;
 /*  (u)int256 addition modulo 2**256
-    Stack no. 00
-    Last update: 28/02
+    Stack no. 01 (op code is 01)
+    Last update: 05/03 
+    Test: Passed Dissasembly test! 05/03
 */
-public class add {
-    
+public class add implements GetInstructionsFromOpcode 
+{
+    private String opcode;
+    private String name;
+   
+    public add(String opcode)
+    {
+        this.opcode=opcode;
+        this.name = "ADD";
+    }
+   
+    public String getOpcode() 
+    {
+        return opcode;
+    }
+   
+    public String getName()
+    {
+        return this.name;
+    }
+   
+    @Override
+    public String accept(Dissasemble visitor) 
+    {
+        return visitor.visit(this);
+    }
+   
 }
